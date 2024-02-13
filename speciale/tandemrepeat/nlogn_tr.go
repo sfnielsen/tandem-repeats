eee70fd0771082092c32ad8c4699cabcdf33aee7
sfnielsen <simonfn7100@gmail.com>
2024-02-13 10:17:00 +0100

    we did most of nlogn but a bug remains

--- speciale/tandemrepeat/nlogn_tr.go
+++ speciale/tandemrepeat/nlogn_tr.go
@@ -4,6 +4,7 @@ import (
 	"speciale/suffixtree"
 )
 
+// addLeafList adds leaflists to the suffix tree
 func addLeafList(st suffixtree.SuffixTree) {
 	root := st.GetRoot()
 
@@ -15,13 +16,14 @@ func addLeafList(st suffixtree.SuffixTree) {
 			node.LeafList = []int{node.Label}
 			return node.LeafList
 		} else {
+			// case for internal node
 			var longest int
-
 			for _, child := range node.Children {
 				if child != nil {
+					//get the leaflist of the child
 					childLeafList := dfs(child)
 					node.LeafList = append(node.LeafList, childLeafList...)
-
+					//keep track of the biggest child found
 					if len(childLeafList) > longest {
 						longest = len(childLeafList)
 						node.BiggestChild = child
@@ -36,38 +38,119 @@ func addLeafList(st suffixtree.SuffixTree) {
 	dfs(root)
 }
 
+// get all tandem repeats
+func getAllTandemRepeats(allBranchingRepeats []TandemRepeat, st suffixtree.SuffixTree) []TandemRepeat {
+	var allTandemRepeats []TandemRepeat = make([]TandemRepeat, 0)
+
+	for _, v := range allBranchingRepeats {
+		// add tandem repeat until length is 0
+		i := 1
+
+		// left rotate until we no longer have a tandem repeat (or we reach the start of the string)
+		for v.Start-i >= 0 {
+			if st.GetInputString()[v.Start-i] == st.GetInputString()[(v.Start-i)+2*(v.length)] {
+				allTandemRepeats = append(allTandemRepeats, TandemRepeat{v.Start - i, i, 2})
+				i += 1
+			} else {
+				break
+			}
+		}
+
+	}
+	allTandemRepeats = append(allTandemRepeats, allBranchingRepeats...)
+	return allTandemRepeats
+}
+
+// GetTandemRepeatSubstring returns the substring of the tandem repeat
+func getIdxtoDfsTable(st suffixtree.SuffixTree) []int {
+	//create table
+	var idxToDfsTable []int = make([]int, len(st.GetInputString()))
+
+	//fill table with another dfs...
+	//this can be done during construction or in another dfs...
+	//but we can optimize it later
+	var dfs func(node *suffixtree.SuffixTreeNode) int
+	dfs = func(node *suffixtree.SuffixTreeNode) int {
+		if node.IsLeaf() {
+			idxToDfsTable[node.Label] = node.DfsInterval.Start
+			return node.DfsInterval.Start
+		} else {
+			for _, child := range node.Children {
+				if child != nil {
+					dfs(child)
+				}
+			}
+		}
+		return 0
+	}
+	dfs(st.GetRoot())
+
+	return idxToDfsTable
+}
+
+// FindTandemRepeatsLogarithmic finds tandem repeats in a suffix tree in O(nlogn) time
 func FindTandemRepeatsLogarithmic(st suffixtree.SuffixTree) []TandemRepeat {
+	//first we need to add the leaflist to the suffix tree
 	addLeafList(st)
+
+	//we create the a idx to dfs mapping
+	idxToDfsTable := getIdxtoDfsTable(st)
+
+	//store all branching repeats
 	var allBranchingRepeats []TandemRepeat = make([]TandemRepeat, 0)
 
+	// now we run stoye and gusfield 'optimized algorithm'
 	var dfs func(node *suffixtree.SuffixTreeNode, depth int)
 	dfs = func(node *suffixtree.SuffixTreeNode, depth int) {
 		depth = depth + node.EdgeLength()
 
 		for _, child := range node.Children {
-			if child != nil || !child.IsLeaf() {
-				// step 1, marking internal nodes is done implicitly by a depth-first traversal
-				dfs(child, depth)
-
-				// step 2a, find the set LL'(v) which is leaf list minus the leaf list of the biggest child
-				var leafListPrime []int
-				for _, child := range node.Children {
-					if child == nil || child.Label == node.BiggestChild.Label {
-						continue
-					}
-					leafListPrime = append(leafListPrime, child.LeafList...)
-				}
-
-				// step 2b
-				//for _, child := range node.Children {
-				//j := child.Label + depth
-
-				//}
-
+			if child == nil {
+				continue
 			}
+			// iterate over elements from leaflistPrime
+			//step 2a is performed implicitly by traversal of the children (minus the biggest child)
+			if node.BiggestChild != child {
+
+				for _, leaf := range child.LeafList {
+
+					//step 2b
+					i := leaf
+					j := i + depth
+					if j < len(st.GetInputString()) {
+						dfsVal := idxToDfsTable[j]
+						//check if j is in dfs interval
+						if dfsVal >= node.DfsInterval.Start && dfsVal <= node.DfsInterval.End {
+
+							//now check if we are branching
+							if i+2*depth < len(st.GetInputString()) && st.GetInputString()[i] != st.GetInputString()[i+2*depth] {
+								//we have a branching repeat
+								allBranchingRepeats = append(allBranchingRepeats, TandemRepeat{leaf, depth, 2})
+							}
+						}
+					}
+					//step 2c
+					j = leaf
+					i = j - depth
+					if i >= 0 && i < len(st.GetInputString()) {
+						dfsVal := idxToDfsTable[i]
+						//check if i is in dfs interval
+						if dfsVal >= node.DfsInterval.Start && dfsVal <= node.DfsInterval.End {
+
+							//now check if we are branching
+							if i+2*depth < len(st.GetInputString()) && st.GetInputString()[i] != st.GetInputString()[i+2*depth] {
+								//we have a branching repeat
+								allBranchingRepeats = append(allBranchingRepeats, TandemRepeat{i, depth, 2})
+							}
+						}
+					}
+				}
+			}
+			// step 1, marking internal nodes is done implicitly by a depth-first traversal
+			dfs(child, depth)
 		}
 	}
 	dfs(st.GetRoot(), 0)
 
-	return allBranchingRepeats
+	return getAllTandemRepeats(allBranchingRepeats, st)
 }
--- speciale/tandemrepeat/nlogn_tr_test.go
+++ speciale/tandemrepeat/nlogn_tr_test.go
@@ -1,10 +1,44 @@
 package tandemrepeat
 
 import (
+	"speciale/stringgenerators"
 	"speciale/suffixtree"
 	"speciale/suffixtreeimpl"
 	"testing"
 )
+
+// Test functions must start with the word "Test" and take a *testing.T parameter.
+var (
+	setupCompleted     bool
+	alphabetGenerator  stringgenerators.StringGenerator
+	randomGenerator    stringgenerators.StringGenerator
+	fibonacciGenerator stringgenerators.StringGenerator
+)
+
+// addLeafList adds leaflists to the suffix tree
+func init() {
+	if !setupCompleted {
+		// Perform setup steps here
+		// ...
+		alphaGen := &stringgenerators.AlphabetStringGenerator{
+			Alphabet: stringgenerators.AlphabetDNA,
+		}
+		alphabetGenerator = alphaGen
+
+		randGen := &stringgenerators.RandomStringGenerator{
+			Alphabet: stringgenerators.AlphabetAB,
+		}
+		randomGenerator = randGen
+
+		fibGen := &stringgenerators.FibonacciStringGenerator{
+			First:  "b",
+			Second: "a",
+		}
+		fibonacciGenerator = fibGen
+
+		setupCompleted = true
+	}
+}
 
 func TestBiggestChildIsBiggestChild(t *testing.T) {
 
@@ -46,3 +80,77 @@ func TestBiggestChildIsBiggestChild(t *testing.T) {
 	}
 	dfs(st.GetRoot())
 }
+
+// test that we find the correct tandem repeats in a simple string
+func TestFindTandemRepeatsLogarithmicVerySimpleExample(t *testing.T) {
+	//define a simple string
+	s := "babakk$"
+	// Create a NaiveSuffixTree instance
+	st := suffixtreeimpl.ConstructNaiveSuffixTree(s)
+	//find the tandem repeats
+	tr := FindTandemRepeatsLogarithmic(st)
+
+	for _, v := range tr {
+		println(GetTandemRepeatSubstring(v, s), v.length)
+	}
+	if len(tr) != 2 {
+		t.Errorf("Expected 2 tandem repeat, got %d", len(tr))
+	}
+	//extract the string from the tandem repeat and verify that it is correct ab$
+	if GetTandemRepeatSubstring(tr[0], s) != "ba" && GetTandemRepeatSubstring(tr[1], s) != "ba" {
+		t.Errorf("Expected tandem repeat to be 'ba'")
+	}
+	if GetTandemRepeatSubstring(tr[0], s) != "k" && GetTandemRepeatSubstring(tr[1], s) != "k" {
+		t.Errorf("Expected tandem repeat to be 'k'")
+
+	}
+}
+
+// test that we can find ALL tandem repeats
+func TestFindTandemRepeatsLogarithmicSimpleExample(t *testing.T) {
+	//generate some big strings from the stringgenerators
+	s := randomGenerator.GenerateString(50)
+	println(s)
+
+	// find tandem repeats with the naive_tr
+	tr1 := FindTandemRepeatsNaive(s)
+
+	tr2 := FindTandemRepeatsLogarithmic(suffixtreeimpl.ConstructNaiveSuffixTree(s))
+
+	//check length are the same
+	if len(tr1) != len(tr2) {
+		t.Errorf("Expected %d tandem repeats, got %d", len(tr1), len(tr2))
+	}
+
+	//make 2 sets and check that they are equal
+	//if not print the difference
+	set1 := make(map[TandemRepeat]bool)
+	for _, v := range tr1 {
+		set1[v] = true
+	}
+	set2 := make(map[TandemRepeat]bool)
+	for _, v := range tr2 {
+		set2[v] = true
+	}
+	//not print difference if they are not equal
+	if len(set1) != len(set2) {
+		mismatches := 0
+		t.Errorf("Sets are not equal. Set1: %d, Set2: %d", len(set1), len(set2))
+		for k, _ := range set1 {
+			if !set2[k] {
+				t.Errorf("Set2 does not contain %v", k)
+				mismatches++
+			}
+		}
+		for k, _ := range set2 {
+			if !set1[k] {
+				t.Errorf("Set1 does not contain %v", k)
+				mismatches++
+			}
+		}
+		t.Errorf("Total mismatches: %d", mismatches)
+	}
+
+}
+
+//AABBAABABABAABAAABABAABBABAAABAABAABABBBAAABBAABAB$
