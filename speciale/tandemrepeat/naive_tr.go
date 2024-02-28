140680ca8efb8a8325a28a9d7c408e8329acbbea
sfnielsen <simonfn7100@gmail.com>
2024-02-28 12:29:57 +0100

    simplified naive_tr

--- speciale/tandemrepeat/naive_tr.go
+++ speciale/tandemrepeat/naive_tr.go
@@ -8,18 +8,14 @@ func FindTandemRepeatsNaive(s string) []TandemRepeat {
 	//keep two pointers, i and j, and a third pointer, k, to keep track of the length of the tandem repeat
 	for i = 0; i < len(s); i++ {
 		for j = i + 1; j < len(s); j++ {
-			if s[i] == s[j] {
-				for k = 0; k < j-i; k++ {
-					//if the characters at i+k and j+k are not the same, we break the loop
-					//as this is not a tandem repeat
-					if s[i+k] != s[j+k] {
-						break
-					}
-				}
-				//if we ran k to the end, we have a tandem repeat
-				if k == j-i {
-					repeats = append(repeats, TandemRepeat{i, k, 2})
-				}
+			k = j - i //length of the tandem repeat
+			if j+k > len(s)-1 {
+				break
+			}
+			if s[i:j] == s[j:j+k] {
+				//add tandem repeat to list
+				repeats = append(repeats, TandemRepeat{i, k, 2})
+
 			}
 		}
 	}
