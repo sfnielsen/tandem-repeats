f9099fe429cbda7bf176de276f1dd5a720634b3e
sfnielsen <simonfn7100@gmail.com>
2024-04-05 10:54:33 +0200

    small change

--- speciale/lce/lce_linear.go
+++ speciale/lce/lce_linear.go
@@ -15,22 +15,24 @@ type LCELinear struct {
 	blocks     [][]int
 	LPrime     []int
 	BPrime     []int
-	LPrimeST   [][]int
+	LPrimeST   *sparseTable
 
 	//backward LCE queries
 	//TBD
 }
+type sparseTable = [][]int
 
 // main function for the LCE linear time preprocessing
 func PreProcessLCE(st suffixtree.SuffixTreeInterface) *LCELinear {
 	//create L,E,R arrays
 	L, E, R := createLERArrays(st)
-
+	// divide L into blocks
 	blocks := createLBlocks(L)
-
+	// compute L' and B'
 	LPrime, BPrime := computeLPrimeandBPrime(blocks)
-
+	// compute sparse table for L'
 	LPrimeSparseTable := computeSparseTable(LPrime)
+	// precompute all possible normalized blocks
 
 	return &LCELinear{&st, L, E, R, blocks, LPrime, BPrime, LPrimeSparseTable}
 }
@@ -127,46 +129,84 @@ func computeLPrimeandBPrime(blocks [][]int) ([]int, []int) {
 }
 
 // function to compute sparse tables (ST) using dynamic programming
-func computeSparseTable(LPrime []int) [][]int {
+func computeSparseTable(LPrime []int) *sparseTable {
 	//compute sparse table
 	n := len(LPrime)
-	sparseTable := make([][]int, n)
+	table := make([][]int, n)
 	for i := 0; i < len(LPrime); i++ {
-		sparseTable[i] = make([]int, int(math.Ceil(math.Log2(float64(n))))+1)
+		table[i] = make([]int, int(math.Ceil(math.Log2(float64(n))))+1)
 
 	}
 
 	//first compute first row (trivial)
 	for i, v := range LPrime {
-		sparseTable[i][0] = v
+		table[i][0] = v
 	}
 
 	//compute the rest of the sparse table
 	for j := 1; 1<<j <= n; j++ {
 		for i := 0; i+(1<<j) <= n; i++ {
-			sparseTable[i][j] = minInt(sparseTable[i][j-1], sparseTable[i+(1<<(j-1))][j-1])
+			table[i][j] = intMin(table[i][j-1], table[i+(1<<(j-1))][j-1])
 		}
 	}
 
-	return sparseTable
+	return &table
 }
 
 // function to compute the LCE between two indices i and j
-func RMQLookup(i, j int, sparseTable [][]int) int {
+func RMQLookup(i, j int, sparseTable *sparseTable) int {
 
 	k := int(math.Floor((math.Log2(float64(j - i)))))
 
-	range1 := sparseTable[i][k]
-	range2 := sparseTable[j-(1<<k)+1][k]
+	range1 := (*sparseTable)[i][k]
+	range2 := (*sparseTable)[j-(1<<k)+1][k]
 
-	return minInt(range1, range2)
+	return intMin(range1, range2)
 
 }
 
 // helper function to find the minimum of two integers
-func minInt(a, b int) int {
+func intMin(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
+}
+
+// compute all normalized blocks
+func computeNormalizedBlocks(blockLength int) []*sparseTable {
+	// Total number of possible blocks (each element can be + or -)
+	totalNormalizedBlocks := 1 << uint(blockLength)
+
+	// Create a 2D slice to store all blocks
+	normalizedBlocks := make([]*sparseTable, totalNormalizedBlocks)
+
+	// Iterate over all possible blocks and initialize their sparse tables
+	for i := range normalizedBlocks {
+		// Initialize the Sparse Table for the current block
+
+		values := convertBinaryToValues(i, blockLength)
+		normalizedBlocks[i] = computeSparseTable(values)
+	}
+
+	return normalizedBlocks
+}
+
+// convertBinaryToValues converts a binary number to a sequence of +1 and -1 values
+func convertBinaryToValues(binaryNumber int, length int) []int {
+	values := make([]int, length)
+
+	// Iterate over each bit position
+	for i := 0; i < length; i++ {
+		// Extract the i-th bit from the binaryNumber
+		bit := (binaryNumber >> uint(i)) & 1
+		// If bit is 0, set the corresponding value to -1, otherwise set it to +1
+		if bit == 0 {
+			values[i] = -1
+		} else {
+			values[i] = 1
+		}
+	}
+
+	return values
 }
